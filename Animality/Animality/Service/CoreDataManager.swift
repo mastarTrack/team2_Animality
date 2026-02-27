a4db15aceb4ef528ba23228273d79ed06633684f
coduhee <vdyjb@ewha.ac.kr>
2026-02-27 15:44:40 +0900

    ♻️refactor: CoreDataManager CRUD 캡슐화 적용 및 모델 분리
    - 데이터 안전성을 위해 Create용/Update용 모델 구조체 분리
    - Core Data 스택 및 내부 로직에 private 접근 제어자 적용 (캡슐화)

--- Animality/Animality/Service/CoreDataManager.swift
+++ Animality/Animality/Service/CoreDataManager.swift
@@ -7,6 +7,32 @@
 
 import CoreData
 
+// MARK: Animal 등록 모델 구조체
+struct CreateAnimalModel {
+    var name: String
+    var category: String
+    var type: String
+    var size: String
+    var latitude: Double
+    var longitude: Double
+    var price: Int32
+    var status: String
+}
+
+// MARK: Animal 수정 모델 구조체
+struct UpdateAnimalModel {
+    var name: String?
+    var category: String?
+    var type: String?
+    var size: String?
+    var latitude: Double?
+    var longitude: Double?
+    var price: Int32?
+    var status: String?
+}
+
+
+// MARK: -- 코어데이터 매니저
 class CoreDataManager {
     
     static let shared = CoreDataManager()
@@ -15,13 +41,12 @@ class CoreDataManager {
     
     // MARK: - Core Data stack
     // persistentContainer 설정
-    lazy var persistentContainer: NSPersistentContainer = {
+    private lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Animality")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // 수정: fatalError 대신 에러를 콘솔에 출력, 및 앱을 계속 가동시킴
+                // 수정: fatalError 대신 에러를 콘솔에 출력하여 에러시에도 앱 중단 X
                 print("CoreData 로드 실페: \(error.localizedDescription)")
-                
                 // Alert로 사용자에 알림창으로 에러 알림하는 로직 추가 예정
             }
         })
@@ -29,43 +54,55 @@ class CoreDataManager {
     }()
     
     // context로 데이터 추출
-    var context: NSManagedObjectContext {
+    private var context: NSManagedObjectContext {
         return persistentContainer.viewContext
     }
     
-    
     // MARK: - Core Data Saving support
     // 변경 사항이 있을 경우 데이터 저장
-    func saveContext () throws {
+    private func saveContext () throws {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             try context.save()
         }
     }
     
+    // saveContext - 메서드
+    func doCatchSaveContext() {
+        do {
+            try self.saveContext()
+            print("데이터 저장 성공")
+        } catch {
+            print("데이터 저장 실패 \(error.localizedDescription)")
+            // Alert로 사용자에 알림창으로 에러 알림하는 로직 추가 예정
+        }
+    }
     
     // MARK: -- CRUD
     
-    // CREATE
+    // MARK: CREATE
     // 개체 등록 - 개체등록 뷰에서 완료 버튼 누르면 호출할 함수
-    func createAnimalEntity(name: String, category: String, type: String, size: String, latitude: Double, longitude: Double, price: Int32){
+    func createAnimalEntity(with payload: CreateAnimalModel){
         let context = self.context
         
         let newEntity = AnimalEntity(context: context)
         
-        newEntity.id = UUID()
-        newEntity.name = name
-        newEntity.category = category
-        newEntity.type = type
-        newEntity.size = size
-        newEntity.latitude = latitude // 위도
-        newEntity.longitude = longitude // 경도
-        newEntity.pricePerHour = price
+        newEntity.id = UUID() // ID는 자동 생성됨
+        newEntity.name = payload.name
+        newEntity.category = payload.category
+        newEntity.type = payload.type
+        newEntity.size = payload.size
+        newEntity.latitude = payload.latitude // 위도
+        newEntity.longitude = payload.longitude // 경도
+        newEntity.pricePerHour = payload.price
+        newEntity.status = payload.status
         
         doCatchSaveContext()
     }
     
-    // READ
+    
+    // MARK: READ
+
     // 1. 저장한 모든 개체 불러오기
     func fetchAllAnimalEntities() -> [AnimalEntity] {
         // AnimalEntity 데이터 요청
@@ -79,7 +116,7 @@ class CoreDataManager {
             return []
         }
     }
-    
+
     // 2. 특정 개체 불러오기
     func fetchOneAnimalEntity(id: UUID) -> AnimalEntity? {
         let request: NSFetchRequest<AnimalEntity> = AnimalEntity.fetchRequest()
@@ -95,23 +132,23 @@ class CoreDataManager {
     }
     
     
-    // UPDATE
-    // 등록한 개체를 상세 정보 뷰에서 수정하기
-    func updateAnimalEntity(entity: AnimalEntity, newName: String?, newCategory: String?, newType: String?, newSize: String?, newLatitude: Double?, newLongitude: Double?, newPrice: Int32?){
+    // MARK: UPDATE
+    func updateAnimalEntity(entity: AnimalEntity, with payload: UpdateAnimalModel) {
         
-        if let name = newName { entity.name = name }
-        if let category = newCategory { entity.category = category }
-        if let type = newType { entity.type = type }
-        if let size = newSize { entity.size = size }
-        if let latitude = newLatitude { entity.latitude = latitude }
-        if let longitude = newLongitude { entity.longitude = longitude }
-        if let price = newPrice { entity.pricePerHour = price }
+        if let name = payload.name { entity.name = name }
+        if let category = payload.category { entity.category = category }
+        if let type = payload.type { entity.type = type }
+        if let size = payload.size { entity.size = size }
+        if let latitude = payload.latitude { entity.latitude = latitude }
+        if let longitude = payload.longitude { entity.longitude = longitude }
+        if let price = payload.price { entity.pricePerHour = price }
+        if let status = payload.status { entity.status = status }
         
         doCatchSaveContext()
     }
     
     
-    // DELETE
+    // MARK: DELETE
     // 등록한 개체를 상세 정보 뷰에서 삭제하기
     func deleteAnimalEntity(entity: AnimalEntity) {
         self.context.delete(entity)
@@ -119,14 +156,4 @@ class CoreDataManager {
         doCatchSaveContext()
     }
     
-    // saveContext - 메서드
-    func doCatchSaveContext() {
-        do {
-            try self.saveContext()
-            print("데이터 저장 성공")
-        } catch {
-            print("데이터 저장 실패 \(error.localizedDescription)")
-            // Alert로 사용자에 알림창으로 에러 알림하는 로직 추가 예정
-        }
-    }
 }
