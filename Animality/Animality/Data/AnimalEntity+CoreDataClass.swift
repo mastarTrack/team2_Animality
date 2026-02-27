e40eda1ea8efae5d67c1d156145ecd3471a4bb31
coduhee <vdyjb@ewha.ac.kr>
2026-02-27 12:13:53 +0900

    ✨feat: AnimalEntity 엔티티 생성

---
+++ Animality/Animality/Data/AnimalEntity+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalEntity+CoreDataClass.swift
+//  Animality
+//
+//  Created by 김주희 on 2/27/26.
+//
+//
+
+public import Foundation
+public import CoreData
+
+public typealias AnimalEntityCoreDataClassSet = NSSet
+
+@objc(AnimalEntity)
+public class AnimalEntity: NSManagedObject {
+
+}
