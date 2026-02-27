7fcb3c7ff28bc883e2897154de801bf1beb949e1
t2025-m0143 <>
2026-02-27 15:38:47 +0900

    ✨feat: 현재 위치 얻기 구현 완료

--- Animality/Animality/View/ViewController/MapViewController.swift
+++ Animality/Animality/View/ViewController/MapViewController.swift
@@ -7,14 +7,20 @@
 import UIKit
 import SnapKit
 import NMapsMap
+import CoreLocation
 
 class MapViewController: UIViewController {
     let mapView = NMFMapView(frame: .zero)
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setMapView()
         setLayout()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 거리 정확도 설정 (설정하지 않을 시 kcLLocationAccuracyBest가 디폴트)
+        checkAuthorizationStatus()
     }
 }
 
@@ -32,5 +38,31 @@ extension MapViewController {
         mapView.snp.makeConstraints {
             $0.edges.equalTo(view.safeAreaLayoutGuide)
         }
+    }
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    // 위치 정보 권한 상태 확인
+    private func checkAuthorizationStatus() {
+        if locationManager.authorizationStatus == .authorizedAlways
+            || locationManager.authorizationStatus == .authorizedWhenInUse { // 위치 권한 허용시(항상 || 앱을 사용하는 동안)
+            locationManager.startUpdatingLocation() // 위치 정보 받기 시작
+        } else if locationManager.authorizationStatus == .notDetermined { // 위치 권한 미지정시
+            locationManager.requestWhenInUseAuthorization() // 권한 요청
+        } else if locationManager.authorizationStatus == .denied {
+            print("위치 권한 거절 상태")
+        }
+    }
+    // startUpdatingLocation이 호출되었을 때 호출되는 함수
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var longitude = CLLocationDegrees() // 경도
+        var latitude = CLLocationDegrees() // 위도
+        
+        if let location = locations.first {
+            longitude = location.coordinate.longitude
+            latitude = location.coordinate.latitude            
+        }
+        print("위도: \(latitude), 경도: \(longitude)")
+        locationManager.stopUpdatingLocation()
     }
 }
