1f28270ba62c4604ede2b1cb7926f4715e0ff2b4
t2025-m0143 <>
2026-02-27 10:45:32 +0900

    🩹chore: 기초 파일 생성 및 gitignore 추가

---
+++ Animality/Animality/Model/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  Animality
+//
+//  Created by t2025-m0143 on 2/27/26.
+//
+import Alamofire
+
+class NetworkManager {
+    
+    private func fetchKeys() {
+        let path = 
+    }
+}
