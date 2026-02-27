1efdb05d8a70edc80eafcf29615102fc00e9725a
hanjuheon <godzx3@gmail.com>
2026-02-27 12:22:30 +0900

    ✨feat: 메인 ViewController 생성 및 TabBar 추가
    - 메인 ViewController 생성
    - TabBer 삽입 기능 추가: 확장성 고려 구현(모델을 이용하여 탭아이템 삽입)
    - TabItem 모델 생성
    
    Refs #59

---
+++ Animality/Animality/Model/TabItem.swift
@@ -0,0 +1,20 @@
+//
+//  TabItem.swift
+//  Animality
+//
+//  Created by Hanjuheon on 2/27/26.
+//
+
+import UIKit
+
+/// 탭바 정의 아이템 모델
+struct TabItem {
+    /// 타이틀
+    let title: String
+    /// 이미지 이름
+    let imageName: String
+    /// 선택됬을때 이미지 이름
+    let selectedImageName: String?
+    /// 뷰컨트롤러 타입 설정
+    let viewControllerType: UIViewController.Type
+}
