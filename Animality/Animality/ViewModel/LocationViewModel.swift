4b34c8d17ffce40bba226e63d0b0f721fb16dde7
t2025-m0143 <>
2026-03-01 23:45:59 +0900

    ♻️refactor: fetchMarker 메서드 수정
    - 좌표에 따른 동물 목록을 저장하는 프로퍼티 coordinates 추가
    	-> 좌표에 따른 동물을 분류하는 메서드 categorizeAnimalByCoordinate 추가
    - 동일 좌표의 경우 하나의 마커만 생성하기 위해 fetchMarker의 반환값 변경

--- Animality/Animality/ViewModel/LocationViewModel.swift
+++ Animality/Animality/ViewModel/LocationViewModel.swift
@@ -34,6 +34,7 @@ class LocationViewModel: ViewModelProtocol {
     func action(_ action: Action) {
         switch action {
         case let .initialized(lat, lng):
+            coordinates = categorizeAnimalByCoordinate()
             let markers = fetchMarkers()
             self.state = .initialized(lat: lat, lng: lng, markers: markers)
             
@@ -50,13 +51,40 @@ class LocationViewModel: ViewModelProtocol {
     // 프로퍼티 선언
     let coreDataManager = TestCoreDataManager()
     
+//    private var animals = [AnimalEntity]() // 동물 배열
+    private var coordinates = [Coordinate: [AnimalEntity]]() // 좌표별 동물 딕셔너리 [좌표: [동물]]
+    
+    private func categorizeAnimalByCoordinate() -> [Coordinate: [AnimalEntity]]{
+        let animals = coreDataManager.fetchAllAnimalEntities()
+
+        return animals.reduce(into: [Coordinate: [AnimalEntity]]()) { dic, animal in
+            let point = Coordinate(latitude: animal.latitude, longtitude: animal.longitude)
+            dic[point, default: []].append(animal)
+        }
+    }
+    
     private func fetchMarkers() -> [(type: String, lat: Double, lng: Double)] {
-            let animals = coreDataManager.fetchAllAnimalEntities()
-        
-        return animals.reduce(into: [(type: String, lat: Double, lng: Double)]()) {
-            guard let type = $1.type else { return }
-            $0.append((type: type, lat: $1.latitude, lng: $1.longitude))
+        return coordinates.reduce(into: [(type: String, lat: Double, lng: Double)]()) { arr, point in
+            let types = point.value.compactMap { $0.type }.sorted()
+            
+            guard !types.isEmpty else { return }
+            
+            if types.count == 1 {
+                arr.append((type: types.first!, lat: point.key.latitude, lng: point.key.longtitude))
+            } else {
+                var typeCount = [String: Int]()
+                for t in types {
+                    typeCount[t, default: 0] += 1
+                    let type = typeCount.sorted(by: { $0.value > $1.value }).first!.key
+                    arr.append((type: type, lat: point.key.latitude, lng: point.key.longtitude))
+                }
+            }
         }
     }
 
+}
+
+struct Coordinate: Hashable {
+    var latitude: Double
+    var longtitude: Double
 }
