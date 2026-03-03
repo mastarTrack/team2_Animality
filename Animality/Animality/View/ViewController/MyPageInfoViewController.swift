05713e3c2a0058370bf197ba04fe7ede8baf5f99
hanjuheon <godzx3@gmail.com>
2026-03-03 20:32:18 +0900

    ♻️refactor: 사용자 정보 화면: 수정기능 로직 수정
    - 사용자 정보 수정 후 수정하기 버튼눌렀을 시, 알람 추가
    - 사용자 정보 수정 관련 버튼 로직 리펙토링

---
+++ Animality/Animality/View/ViewController/MyPageInfoViewController.swift
@@ -0,0 +1,293 @@
+//
+//  MyPageViewController.swift
+//  Animality
+//
+//  Created by Hanjuheon on 3/2/26.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+
+class MyPageInfoViewController : UIViewController {
+    
+    //MARK: - ViewModel
+    
+    //MARK: - Components
+    /// 유저 타이틀 이미지
+
+    private let titleImage = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 40
+        $0.layer.borderWidth = 0.5
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
+    }
+    /// 유저 타이틀 아이디 라벨
+    private let titleNameLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    /// 유저 아이디 라벨
+    private let idLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    /// 유저 이름 텍스트 필드
+    private let nameField = UITextField().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    /// 유저 이메일 텍스트 필드
+    private let emailField = UITextField().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    /// 유저 가입날짜 라벨
+    private let registLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+    /// 유저 렌탈 횟수 라벨
+    private let rentCountLabel = UILabel().then {
+        $0.font = .boldSystemFont(ofSize: 20)
+    }
+
+    /// 사용자 정보 수정버튼
+    private let modifyButton = UIButton().then {
+        $0.setTitle("사용자 정보 수정", for: .normal)
+        $0.setTitle("수정 완료", for: .selected)
+        $0.backgroundColor = .white
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+    }
+
+    /// 등록 내역 버튼
+    private let rentRegistListButton = UIButton().then {
+        $0.setTitle("등록 내역", for: .normal)
+        $0.backgroundColor = .white
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+    }
+    /// 수정 취소 버튼
+    private let modifyCancelButton = UIButton().then {
+        $0.setTitle("수정 취소", for: .normal)
+        $0.backgroundColor = .white
+        $0.setTitleColor(.systemGray, for: .normal)
+        $0.layer.cornerRadius = 10
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.systemGray.cgColor
+        $0.isHidden = true
+    }
+    
+    //MARK: - Closures
+    /// 수정 완료 클로저
+    var approveModifyClosure: (()->Void)?
+    /// 수정 취소 클로져
+    var cancelModifyClosure: (()->Void)?
+    
+    //MARK: - Init
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        bindButtonAction()
+    }
+    
+}
+//MARK: - METHOD: Button Action Binding
+extension MyPageInfoViewController {
+    func bindButtonAction() {
+        modifyButton.addAction( UIAction { [weak self] _ in
+            guard let self else { return }
+            if self.modifyButton.isSelected {
+                // 저장/종료 시점 콜백
+                let alert = UIAlertController(title: "정보 수정 확인", message: "수정한 정보를 저장하시겠습니까?", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "취소", style: .cancel))
+                alert.addAction(UIAlertAction(title: "수정", style: .destructive) { _ in
+                    self.modifyButton.isSelected.toggle()
+                    self.setEditingMode(self.modifyButton.isSelected)
+                    self.approveModifyClosure?()
+                })
+                present(alert, animated: true)
+            } else {
+                self.modifyButton.isSelected.toggle()
+                self.setEditingMode(self.modifyButton.isSelected)
+            }
+            
+            }, for: .touchUpInside )
+        
+        modifyCancelButton.addAction( UIAction { [weak self] _ in
+            guard let self else { return }
+            self.setEditingMode(false)
+            self.cancelModifyClosure?()
+        }, for: .touchUpInside )
+        
+        rentRegistListButton.addAction( UIAction { [weak self] _ in
+            guard let self else { return }
+            // TODO: 등록 목록 화면 전환 코드 추가 예정
+        }, for: .touchUpInside )
+    }
+}
+
+//MARK: - METHOD: Update UI
+extension MyPageInfoViewController {
+    /// UI 업데이트 메소드
+    func updateUI(image: UIImage, id: String, name: String, email: String, registDate: Date, rentCount: Int) {
+        titleImage.image = image
+        titleNameLabel.text = name
+        nameField.text = name
+        idLabel.text = id
+        emailField.text = email
+        registLabel.text = registDate.formatted()
+        rentCountLabel.text = String(rentCount)
+    }
+    
+    /// 사용자 정보 수정 메소드
+    private func setEditingMode(_ isEditing: Bool) {
+        modifyButton.isSelected = isEditing
+
+        [nameField, emailField].forEach { field in
+            field.isUserInteractionEnabled = isEditing
+            field.borderStyle = isEditing ? .roundedRect : .none
+            field.backgroundColor = isEditing ? .secondarySystemBackground : .clear
+        }
+
+        modifyCancelButton.isHidden = !isEditing
+        rentRegistListButton.isHidden = isEditing
+
+        if !isEditing { view.endEditing(true) }
+    }
+}
+
+//MARK: - MATHOD: Configure UI
+extension MyPageInfoViewController {
+    private func configureUI() {
+
+        let scrollView = UIScrollView()
+        let contentView = UIView()
+        let stackView = UIStackView().then {
+            $0.axis = .vertical
+            $0.spacing = 15
+            $0.alignment = .center
+            
+        }
+        
+        let titleView = UIView().then{
+            $0.backgroundColor = .deepSerenity
+            $0.layer.cornerRadius = 40
+            $0.layer.borderWidth = 0.5
+            $0.layer.borderColor = UIColor.systemGray6.cgColor
+        }
+        titleView.addSubview(titleImage)
+        titleView.addSubview(titleNameLabel)
+
+        
+        let ui = [idLabel, nameField, emailField, registLabel, rentCountLabel]
+        
+        let titleLabels = ["아이디", "이름", "이메일", "가입 날짜", "렌탈 횟수"].map{
+            title -> UILabel in
+            let label = UILabel().then {
+                $0.text = title
+                $0.font = .systemFont(ofSize: 14)
+                $0.textColor = .systemGray
+                $0.textAlignment = .left
+            }
+            return label
+        }
+        
+        let stackViews = (0..<ui.count).map { _ -> UIStackView in
+            let stackView = UIStackView().then {
+                $0.backgroundColor = .systemGray6
+                $0.axis = .horizontal
+                $0.alignment = .center
+                $0.layer.cornerRadius = 10
+                $0.isLayoutMarginsRelativeArrangement = true
+                $0.layoutMargins = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+            }
+            return stackView
+        }
+        
+        stackView.addArrangedSubview(titleView)
+        for i in 0..<ui.count {
+            stackViews[i].addArrangedSubview(titleLabels[i])
+            stackViews[i].addArrangedSubview(ui[i])
+            stackView.addArrangedSubview(stackViews[i])
+        }
+        
+        stackView.addArrangedSubview(modifyButton)
+        stackView.addArrangedSubview(rentRegistListButton)
+        stackView.addArrangedSubview(modifyCancelButton)
+        
+        contentView.addSubview(stackView)
+        scrollView.addSubview(contentView)
+        view.addSubview(scrollView)
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(15)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().inset(30)
+            $0.bottom.equalToSuperview().inset(16)
+        }
+        
+        scrollView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.edges.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
+        }
+        
+        titleView.snp.makeConstraints {
+            $0.height.equalTo(180)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+
+        titleImage.snp.makeConstraints {
+            $0.height.width.equalTo(80)
+            $0.top.equalToSuperview().offset(35)
+            $0.centerX.equalToSuperview()
+        }
+        titleNameLabel.snp.makeConstraints {
+            $0.top.equalTo(titleImage.snp.bottom).offset(10)
+            $0.centerX.equalToSuperview()
+        }
+        
+        
+        for stack in 0..<stackViews.count {
+            stackViews[stack].snp.makeConstraints{
+                $0.height.equalTo(50)
+                $0.leading.equalToSuperview()
+                $0.trailing.equalToSuperview()
+            }
+        }
+        
+        modifyButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        rentRegistListButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+        modifyCancelButton.snp.makeConstraints {
+            $0.height.equalTo(50)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+        
+    }
+}
+
+@available(iOS 17.0, *)
+#Preview {
+    let vc = MyPageInfoViewController()
+    let image = UIImage()
+    vc.updateUI(image: image, id: "godzx3", name: "한주헌", email: "xxx@xxxxx.xxx", registDate: Date(), rentCount: 0)
+    return vc
+}
