698957bea41d6450b858c48c3a4ff1805bb17af2
t2025-m0143 <>
2026-03-05 00:29:01 +0900

    ✨feat: sheetView UICollectionView 초기 세팅

--- Animality/Animality/View/View/PinSheetView.swift
+++ Animality/Animality/View/View/PinSheetView.swift
@@ -7,4 +7,40 @@
 import UIKit
 
 class PinSheetView: UISheetPresentationController {
+    private lazy var listView = UICollectionView(frame: .zero, collectionViewLayout: makeCompositionalLayout())
+    private lazy var dataSource = makeCollectionViewDiffableDataSource(listView)
+    
+    // 레이아웃 설정
+    private func makeCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { sectionIndex, environment in
+            var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+            
+            // 섹션 배경색 설정
+            configuration.backgroundColor = .clear
+            
+            let section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: environment)
+            return section
+        }
+    }
+    
+    // DiffableDataSource 설정
+    private func makeCollectionViewDiffableDataSource(_ collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Int, Animal> {
+        let listCellRegistration = UICollectionView.CellRegistration<SheetAnimalCell, Animal> { (cell, indexPath, item) in
+            cell.configure()
+        }
+        
+        let dataSource = UICollectionViewDiffableDataSource<Int, Animal>(collectionView: listView) { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: itemIdentifier)
+        }
+        
+        return dataSource
+    }
+    
+    // 스냅샷 설정
+    private func setSnapshot(with data: [Animal]) {
+        var snapShot = NSDiffableDataSourceSnapshot<Int, Animal>()
+        snapShot.appendSections([0])
+        snapShot.appendItems(data, toSection: 0)
+        self.dataSource.apply(snapShot)
+    }
 }
---
+++ Animality/Animality/View/View/SheetAnimalCell.swift
@@ -0,0 +1,14 @@
+//
+//  SheetAnimalCell.swift
+//  Animality
+//
+//  Created by t2025-m0143 on 3/5/26.
+//
+
+import UIKit
+
+final class SheetAnimalCell: UICollectionViewListCell {
+    func configure() {
+        
+    }
+}
