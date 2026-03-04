d5b58e567205b81ecded953f6d21471f71d423d0
t2025-m0143 <>
2026-03-05 02:02:12 +0900

    ✨feat: Pin SheetView UI 설정

--- Animality/Animality/View/View/PinSheetView.swift
+++ Animality/Animality/View/View/PinSheetView.swift
@@ -7,35 +7,69 @@
 import UIKit
 
 class PinSheetView: UIViewController {
-    private lazy var listView = UICollectionView(frame: .zero, collectionViewLayout: makeCompositionalLayout())
-    private lazy var dataSource = makeCollectionViewDiffableDataSource(listView)
+    private let animals: [Animal]
     
+    private lazy var animalCollectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+    private lazy var dataSource = makeCollectionViewDiffableDataSource(animalCollectionView)
+    
+    init(animals: [Animal]) {
+        self.animals = animals
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     // 레이아웃 설정
-    private func makeCompositionalLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { sectionIndex, environment in
-            var configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-            
-            // 섹션 배경색 설정
-            configuration.backgroundColor = .clear
-            
-            let section = NSCollectionLayoutSection.list(using: configuration, layoutEnvironment: environment)
-            return section
-        }
+    private func makeCollectionViewLayout() -> UICollectionViewLayout {
+      let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        
+      return UICollectionViewCompositionalLayout(sectionProvider: { sectionIndex, environment in
+          // 여백 설정
+          let spacing: CGFloat = 12
+          
+          // CollectionView 사이즈
+          let containerSize = environment.container.effectiveContentSize
+          
+          // Item 설정
+          let itemSize = containerSize.width
+          let item = NSCollectionLayoutItem(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(itemSize),
+                heightDimension: .absolute(itemSize * 0.3)
+            )
+          )
+          
+          // Group 설정
+          let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .absolute(itemSize),
+                heightDimension: .fractionalHeight(1)
+            ),
+            subitems: [item]
+          )
+          
+          group.interItemSpacing = .fixed(spacing)
+          
+          // Section 설정
+          let section = NSCollectionLayoutSection(group: group)
+          return section
+      }, configuration: configuration)
     }
     
     // DiffableDataSource 설정
     private func makeCollectionViewDiffableDataSource(_ collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Int, Animal> {
-        let listCellRegistration = UICollectionView.CellRegistration<SheetAnimalCell, Animal> { (cell, indexPath, item) in
-            cell.configure()
+        let cellRegistration = UICollectionView.CellRegistration<SheetAnimalCell, Animal> { (cell, indexPath, item) in
+            cell.configure(with: item)
         }
         
-        let dataSource = UICollectionViewDiffableDataSource<Int, Animal>(collectionView: listView) { collectionView, indexPath, itemIdentifier in
-            return collectionView.dequeueConfiguredReusableCell(using: listCellRegistration, for: indexPath, item: itemIdentifier)
+        let dataSource = UICollectionViewDiffableDataSource<Int, Animal>(collectionView: animalCollectionView) { collectionView, indexPath, itemIdentifier in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
         }
         
         return dataSource
