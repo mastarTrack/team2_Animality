4dfbbaf1ca8bd52c7860acd0a9024c7c402bcafe
t2025-m0143 <>
2026-03-04 10:38:50 +0900

    ✨feat: 지역 정보 모델 생성

---
+++ Animality/Animality/Model/LocationInfo.swift
@@ -0,0 +1,15 @@
+//
+//  SearchedAddress.swift
+//  Animality
+//
+//  Created by t2025-m0143 on 3/4/26.
+//
+
+struct LocationInfo {
+    var name: String
+    var address: String
+    var telephone: String
+    var mapX: Double
+    var mapY: Double
+    var image: String?
+}
